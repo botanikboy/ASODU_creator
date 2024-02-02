fd7a4db03b624a3218eca53f31e8acbc62ca10c4
Savinkin Ilya <ISavinkin@croc.ru>
2024-02-02 14:44:52 +0300

    settings and compose yaml for deploy

--- ASODU/ASODU/settings.py
+++ ASODU/ASODU/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default=get_random_secret_key())
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -62,7 +62,7 @@ WSGI_APPLICATION = 'ASODU.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'NAME': os.getenv('POSTGRES_DB', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
         'HOST': os.getenv('DB_HOST', default='localhost'),
@@ -105,7 +105,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-CSRF_TRUSTED_ORIGINS = ['http://localhost/*']
+CSRF_TRUSTED_ORIGINS = ['https://159.89.213.155/*']
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
