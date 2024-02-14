83603c11c8c7c031f19a53588c54e6bf8318c843
Savinkin Ilya <ISavinkin@croc.ru>
2024-02-14 16:21:32 +0300

    .env example update

--- ASODU/ASODU/settings.py
+++ ASODU/ASODU/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default=get_random_secret_key())
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG_MODE', default=True)
 
 ALLOWED_HOSTS = ['*']
 
