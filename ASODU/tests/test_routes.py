d7e8f551aef5705c794cb284ec6ef1a6bf42be78
Ilya Savinkin <iasavinkin@gmail.com>
2024-11-14 19:11:38 +0300

    base fixtures and routes tests added

--- ASODU/tests/test_routes.py
+++ ASODU/tests/test_routes.py
@@ -1,13 +1,60 @@
 from http import HTTPStatus
 
+import pytest
 from pytest_django.asserts import assertRedirects
 from django.urls import reverse
 
 
-def test_home_redirects_to_login_for_anonymous_user(client):
-    url = reverse('panels:index')
-    login_url = reverse('login')
-    expected_url = f'{login_url}?next={url}'
-    response = client.get(url)
-    assert response.status_code == HTTPStatus.FOUND
-    assertRedirects(response, expected_url)
+@pytest.mark.parametrize(
+    'param_client, expected_status',
+    (
+        ('client', HTTPStatus.FOUND),
+        ('author_client', HTTPStatus.OK)
+    )
+)
+@pytest.mark.parametrize(
+    'name, data',
+    (
+        ('index', None),
+        ('template_list', None),
+        ('project_list', None),
+        ('project_detail', 'project'),
+        ('project_edit', 'project'),
+        ('project_create', None),
+        ('panel_detail', 'panel'),
+        ('panel_create', 'project'),
+        ('boq_download_project', 'project'),
+        ('panel_edit', 'panel'),
+        ('panel_edit_contents', 'panel'),
+        ('panel_copy', 'panel'),
+        ('boq_download_panel', 'panel'),
+        ('file_add', 'panel'),
+    )
+)
+def test_any_page_redirects_to_login_for_anonymous_user(
+        client, name, data, request, param_client, expected_status):
+    if data is not None:
+        data = (request.getfixturevalue(data).pk,)
+    url = reverse(f'panels:{name}', args=data)
+    param_client = request.getfixturevalue(param_client)
+    response = param_client.get(url)
+    assert response.status_code == expected_status
+    if expected_status == HTTPStatus.FOUND:
+        login_url = reverse('users:login')
+        expected_redirect_url = f'{login_url}?next={url}'
+        assertRedirects(response, expected_redirect_url)
+
+
+@pytest.mark.parametrize(
+    'name, status_code',
+    (
+        ('users:signup', HTTPStatus.OK),
+        ('users:login', HTTPStatus.OK),
+        ('users:password_change_done', HTTPStatus.FOUND),
+        ('users:password_reset_done', HTTPStatus.OK),
+        ('users:password_reset_complete', HTTPStatus.OK)
+    )
+)
+def test_users_service_pages_availible(client, name, status_code):
+    response = client.get(reverse(name))
+    assert response.status_code == status_code
